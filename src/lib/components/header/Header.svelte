8f9d3e3e5be1eba0e9756f362b09718e0c2690b2
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2024-08-14 19:57:04 -0700

    Added header section with Grids for title and buttons

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,43 @@
+<script>
+
+</script>
+<style>
+.header {
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+  padding: .1vw;
+  column-gap: .5vw;
+  width: 100%;
+}
+
+.verity-title {
+  margin: 1vw;
+  text-align: center;
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: .5vw;
+}
+
+.github-button, .theme-toggle {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.empty {
+
+}
+</style>
+
+<div class="header">
+  <div class="empty"></div>
+  <h1 class="verity-title">Verity Helper (Inside)</h1> 
+  <div class="buttons">
+    <a class="github-button">Source</a>
+    <button class="theme-toggle">Dark/Light Mode</button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,18 @@
 <script>
   import ShapeSelector from "../lib/components/StatueShapeSelector/StatueSelector.svelte";
+  import Header from "../lib/components/header/Header.svelte";
 </script>
 <style>
   .container {
     display: flex;
-    flex-direction: center;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
   }
 </style>
 
 <div class="container">
+  <Header />
   <ShapeSelector />
 </div>
 
