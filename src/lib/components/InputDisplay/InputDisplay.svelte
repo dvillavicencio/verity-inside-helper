5f74c1755ca34c7bb573a21e085a095b0021a142
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2024-08-27 02:06:24 -0700

    Added input validation for shape selector

--- src/lib/components/InputDisplay/InputDisplay.svelte
+++ src/lib/components/InputDisplay/InputDisplay.svelte
@@ -6,7 +6,7 @@ import { fly } from "svelte/transition";
 import { Shape } from "../../Enums/Shape.ts";
 import { StatueLocation } from "../../Enums/StatueLocation.ts";
 import type { InputData } from "../../Types/InputData.ts";
-import { createEventDispatcher, tick } from "svelte";
+import { createEventDispatcher, tick, onMount } from "svelte";
 
 const RESET_EVENT: String = "reset";
 const dispatch = createEventDispatcher();
@@ -16,58 +16,82 @@ let statueShape: Shape = null;
 let statueLocation: StatueLocation = null;
 
 let resetEnabled: boolean = false;
+let outputVisible: boolean = false;
+let inputIsValid: boolean = true;
+let errorMessageVisible: boolean = false;
 
 let outputContainer;
 let inputContainer;
+let container;
 
-function defineSteps(location: StatueLocation, statueShape: Shape, shapes: Shape[]): string[] { 
+function defineSteps(location: StatueLocation, statueShape: Shape, shapes: Shape[]): string[] {
   let doubled: boolean = shapes[0] === shapes[1];
-
   const otherLocations: StatueLocation[] = getOtherLocations(location);
   const otherShapes: Shape[] = getOtherShapes(statueShape);
   const resultingShape: Shape = getResultingShape(statueShape);
   if(!doubled) {
     const value = shapes.find(s => s !== statueShape);
-    return [`KILL a knight until it drops a <b>${value}</b>`, 
-      `DEPOSIT the <b>${value}</b> on the statue holding a <b>${value}</b>`,
-      `WAIT until the other two guardians have double shapes as well`,
-      `KILL another knight and DEPOSIT the <b>${statueShape}</b> on <b>${otherLocations[0]}</b>`,
-      `KILL an ogre if there is one`,
-      `KILL a knight until it drops a second <b>${statueShape}</b>`,
-      `DEPOSIT the <b>${statueShape}</b> on <b>${otherLocations[1]}</b>`,
-      `Up in the wall you should have ONLY one <b>${otherShapes[0]}</b> and one <b>${otherShapes[1]}</b>`,
-      `KILL knights and ogres and pick up both <b>${otherShapes[0]}</b> and <b>${otherShapes[1]}</b>`,
+    return [`<b>KILL</b> a knight until it drops a <b>${value}</b>`, 
+      `<b>DEPOSIT</b> the <b>${value}</b> on the statue holding a <b>${value}</b>`,
+      `<b>WAIT</b> until the other two guardians have double shapes as well`,
+      `<b>KILL</b> another knight and <b>DEPOSIT</b> the <b>${statueShape}</b> on <b>${otherLocations[0]}</b>`,
+      `<b>KILL</b> an ogre if there is one`,
+      `<b>KILL</b> a knight until it drops a second <b>${statueShape}</b>`,
+      `<b>DEPOSIT</b> the <b>${statueShape}</b> on <b>${otherLocations[1]}</b>`,
+      `Up in the wall you should have <b>ONLY</b> one <b>${otherShapes[0]}</b> and one <b>${otherShapes[1]}</b>`,
+      `<b>KILL</b> knights and ogres and pick up both <b>${otherShapes[0]}</b> and <b>${otherShapes[1]}</b>`,
       `If done correctly, you should now be holding a <b>${resultingShape}</b> (${otherShapes[0]} + ${otherShapes[1]})`,
       `Once dissection outside is finished, <b>ESCAPE</b> through the crystal wall at the back of the room`
     ]; 
   } else {
     return [
       `<b>WAIT</b> until the other two guardians have double shapes`,
-      `KILL a knight and PICK a <b>${statueShape}</b>`,
-      `DEPOSIT <b>${statueShape}</b> on <b>${otherLocations[0]}</b>`,
-      `KILL another knight until it drops a <b>${statueShape}</b>`,
-      `DEPOSIT <b>${statueShape}</b> on <b>${otherLocations[1]}</b>`,
+      `<b>KILL</b> a knight and <b>PICK</b> a <b>${statueShape}</b>`,
+      `<b>DEPOSIT</b> <b>${statueShape}</b> on <b>${otherLocations[0]}</b>`,
+      `<b>KILL</b> another knight until it drops a <b>${statueShape}</b>`,
+      `<b>DEPOSIT</b> <b>${statueShape}</b> on <b>${otherLocations[1]}</b>`,
       `Up in the wall you should have ONLY one <b>${otherShapes[0]}</b> and one <b>${otherShapes[1]}</b>`,
-      `KILL knights and ogres and pick up both <b>${otherShapes[0]}</b> and <b>${otherShapes[1]}</b> at the same time`,
+      `<b>KILL</b> knights and ogres and pick up both <b>${otherShapes[0]}</b> and <b>${otherShapes[1]}</b> at the same time`,
       `If done correctly, you should not be holding a <b>${resultingShape}</b> (${otherShapes[0]} + ${otherShapes[1]})`,
       `Once dissection outside is finished, <b>ESCAPE</b> through the crystal wall at the back of the room`
     ];
   }
 } 
 
+function validateAndUpdateOutput() {
+  inputIsValid = validateInput(statueShape, shapes);
+  let params = statueLocation !== null && statueShape !== null && shapes.length == 2;
+  if(params && inputIsValid) {
+    outputVisible = true;
+    errorMessageVisible = false;
+  } else if(params && !inputIsValid) {
+    outputVisible = false;
+    errorMessageVisible = true;
+  }
+}
+
+function validateInput(statueShape: Shape, shapesHeld: Shape[]): boolean {
+  let valid = shapesHeld.filter(item => item === statueShape);
+  console.log(valid);
+  return statueShape !== null && shapesHeld.length > 0 && valid.length != 0;
+}
+
 function setLocation(event) { 
   statueLocation = event.detail;
   resetEnabled = true;
+  validateAndUpdateOutput(); 
 }
 
 function setStatueShape(event) {
   statueShape = event.detail;
   resetEnabled = true;
+  validateAndUpdateOutput();
 }
 
 function setShapes(event) {
   shapes = event.detail;
   resetEnabled = true;
+  validateAndUpdateOutput(); 
 }
 
 function getOtherShapes(shape: Shape): Shape[]{
@@ -92,7 +116,7 @@ function getResultingShape(shape: StatueShape): Shape {
     case Shape.Circle:
       return Shape.Prism;
     default:
-      return new Error("Shape passed in is not a 2D shape");
+      return new Error("Shape passed is not a 2D shape");
   }
 }
 
@@ -110,7 +134,10 @@ function getOtherLocations(currentLocation: StatueLocation): StatueLocation[] {
 }
 
 function reset() {
+  inputIsValid = true;
   resetEnabled = false;
+  outputVisible = false;
+  errorMessageVisible = false;
   statueLocation = null;
   statueShape = null;
   shapes = [];
@@ -126,32 +153,40 @@ function outputTransition(node) {
   }
 }
 
-async function scrollToOutput() {
-  await tick();
+function scrollToOutput() {
   if(outputContainer) {
     outputContainer.scrollIntoView({ behavior: "smooth" })
   }
 }
 
 $: if(outputContainer && window.innerWidth <= 480) {
-  scrollToOutput();  
+  scrollToOutput();
 }
 </script>
 
-<div class="container">
+<div class="container" bind:this={container}> 
   <div class="input-container" bind:this={inputContainer}>
+    {#if errorMessageVisible}
+      <div class="error-container" transition:outputTransition>
+        <p class="error-message">
+          The shapes you put in are wrong.
+          <br />
+          If your statue is holding a {statueShape} you can never have {getOtherShapes(statueShape)[0]} and {getOtherShapes(statueShape)[1]} in your wall
+        </p>
+      </div>
+    {/if}
     <StatueLocationSelector on:statueSelect={setLocation} {resetEnabled}/>
     <StatueShapeSelector on:selectShape={setStatueShape} {resetEnabled}/>
     <ShapeSelector on:shapes={setShapes} {resetEnabled}/>
   </div>
-{#if statueLocation !== null && statueShape !== null && shapes.length == 2} 
-  <div class="output-container" bind:this={outputContainer} transition:outputTransition> 
-    <h3 class="title">Steps:</h3>
-    {#each defineSteps(statueLocation, statueShape, shapes) as step, i}
-      <p>{++i}. {@html step}</p> 
-    {/each} 
-  </div>
-{/if}
+  {#if outputVisible}
+    <div class="output-container" bind:this={outputContainer} transition:outputTransition> 
+      <h3 class="title">Steps:</h3>
+      {#each defineSteps(statueLocation, statueShape, shapes) as step, i}
+        <p>{++i}. {@html step}</p> 
+      {/each} 
+    </div>
+  {/if}
 </div>
 {#if resetEnabled}
   <button class="reset" on:click={reset}>reset</button>
@@ -180,6 +215,19 @@ $: if(outputContainer && window.innerWidth <= 480) {
   align-content: start;
   justify-content: flex-start;
   flex-wrap: wrap;
+}
+
+.error-container {
+  display: flex;
+  flex-direction: column;
+  gap: .5vw;
+  align-items: center;
+}
+
+.error-message {
+  color: red;
+  font-size: 0.83rem;
+  text-align: center;
 }
 
 .title {
