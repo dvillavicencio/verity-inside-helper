389d010ee03c6e5bfe0a964e7611803f372dcb3b
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2024-09-25 03:11:00 -0700

    Added description in the about section for fast-strategy

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -21,8 +21,24 @@
     <p>
       If you and your other fireteam members able to answer those three questions then you are on the right track!
     </p>
-    <h3>> Can this explain challenge mode?</h3>
+    <h3>> Can this tool explain challenge mode?</h3>
     <p>Not yet</p>
+    <h3>> Can this tool explain the fast-strategy?</h3>
+    <p>Yes it can, click on the hamburger menu and check the <i>fast strategy</i> toggle</p>
+    <h3>> What is the fast-strategy in Verity?</h3>
+    <p>
+      The fast-strategy in Verity is just another way of completing the puzzle in which all the three people that are inside the rooms
+      <b>only pass their shapes twice</b>. For this specific strategy there's three possible scenarios that can happen and on each one
+      guardians will be doing slightly different actions depending on their statue and the shapes they have up on their wall.
+      The three scenarios that can happen are:</p>
+      <ol type="1">
+        <li>All guardians have double shapes on their wall</li> 
+        <li>Only one guardian has double shapes on their wall and the two others don't</li>
+        <li>None of the guardians have double shapes on their wall</li>
+      </ol>
+    <p>
+      Regardless of this, the fast-strategy guarantess you'll only pass your shapes <b>twice</b>, only killing one ogre and one set of knights if done properly
+    </p>
   </div>
 </div>
 <style>
